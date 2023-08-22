5b6afb5e94c10f403db5ca66945d92e2ca54675f
trung <trung150502@gmail.com>
2023-08-22 21:36:04 +0700

    change dependencies for new component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NoteList } from '../note-list/note-list.component';
+
 @NgModule({ // each component must belong to an NG module in order to be available to others
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoteList,
   ],
   providers: [],
   bootstrap: [
