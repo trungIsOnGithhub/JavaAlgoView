0fd368b40b8d0119a2503e3c51f9316d0cc0ec2a
trung1 <trung1@gmail.com>
2023-09-04 20:49:12 +0700

    fixing input directive for data inheritance

--- client/src/note-list/note-list.component.ts
+++ client/src/note-list/note-list.component.ts
@@ -12,16 +12,26 @@ import { CommonModule } from '@angular/common';
 export class NoteList {
   contentToAdd: string = "";
 
-  @Input() titleSlug = '';
-  @Input() apiBaseURL = '';
-  @Input() notes = [];
+  @Input() titleSlug :string = '';
+  @Input() apiBaseURL :string = '';
+  @Input() notes :string[] = [];
 
   onKey(event: any) {
     // this.contentToAdd = event.target.value;
     const content = ((document.getElementById("contentToAdd") as HTMLInputElement).value);
     console.log(content);
-    fetch(`${this.apiBaseURL}/${this.titleSlug}/note`)
+    console.log(`${this.apiBaseURL}/${this.titleSlug}/note`);
+    fetch(`${this.apiBaseURL}/${this.titleSlug}/note`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        content: content
+      })
+    })
     .then(res => res.json())
-    .then(json => console.log(json));
+    .then(json => console.log(json))
+    .catch(error => console.error(error));
   }
 }
